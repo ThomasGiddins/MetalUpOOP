c92115d6c274bf5c74b38ea74785c77d38d593be
Thomas Giddins <08ThomasGiddins@cloud.sch.im>
2021-05-17 23:08:01 +0100

    Exercise 22 Completed

--- OOPDraw/OOPDraw/OOPDraw.Designer.cs
+++ OOPDraw/OOPDraw/OOPDraw.Designer.cs
@@ -109,7 +109,9 @@
             this.Shape.FormattingEnabled = true;
             this.Shape.Items.AddRange(new object[] {
             "Line",
-            "Rectangle"});
+            "Rectangle",
+            "Ellipse",
+            "Circle"});
             this.Shape.Location = new System.Drawing.Point(22, 239);
             this.Shape.Name = "Shape";
             this.Shape.Size = new System.Drawing.Size(184, 21);
--- OOPDraw/OOPDraw/OOPDraw.cs
+++ OOPDraw/OOPDraw/OOPDraw.cs
@@ -46,6 +46,12 @@ namespace OOPDraw
                 case "Rectangle":
                     shapes.Add(new Rectangle(currentPen, e.X, e.Y));
                     break;
+                case "Ellipse":
+                    shapes.Add(new Ellipse(currentPen, e.X, e.Y));
+                    break;
+                case "Circle":
+                    shapes.Add(new Circle(currentPen, e.X, e.Y));
+                    break;
             }
         }
         private void Canvas_MouseMove(object sender, MouseEventArgs e)
